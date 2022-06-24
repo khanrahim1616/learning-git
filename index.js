97d7e2e4ac8941e10c6a0a5f986194c63c9754db
khanrahim1616 <khanrahim1616@gmail.com>
2022-06-24 21:12:54 +0500

    changes

---
+++ index.js
@@ -0,0 +1,625 @@
+// // // vbriable-naming-practice
+// // var myname  ="Abdul rahim";
+// // console.log(myname);
+// //  console.log(typeof(myname));
+
+// //  var myage = 23 ;
+// // console.log(myage);
+// // console.log(typeof(myage));
+// // console.log(10 + "20");
+// // console.log(10 - "5"); bug
+// // console.log("Java " + "script");
+// // ("Java " - "script");NaN
+// //  console.log("false" - "true") ;
+// // var iAmuseless = null; bugitsnotobject
+// // console.log(iAmuseless);
+// // console.log(typeof(iAmuseless));
+// // var iAmstandby;
+// // console.log(iAmstandby);
+// // // // // // console.log(typeof(iAmstandby));
+
+// // // // // // var myPhonenumber = 363793828;
+// // // // // // var myName        ="Abdul Rahim "
+// // // // // // console.log(isNaN(myPhonenumber))
+// // // // // // console.log(isNaN(myName))
+// // // // // // if(isNaN(myName)){
+// // // // // //     console.log("plz enter valid phone no.")
+// // // // // // }
+// // console.log("Remainder operator " + 27%4)
+
+// // var num =15;
+// // var newnum=num++ +5;
+// // console.log(num);
+// // console.log(newnum);
+
+// // // // // // var a = 18 ;
+// // // // // // var b = 15 ;
+// // // // // // console.log(a == b)
+// // // // // // console.log(a <b)
+
+// // alert("hello world")
+
+// // // // // // document.write("Raheem Bhai Zindabad")
+
+// // // // // // console.log(3**4)
+
+// // // // // // var a = 5 ;
+// // // // // // var b = 10 ;
+
+// // // // // // // output b=5; a=10
+// // // // // //  c=b      //c=10;
+// // // // // //  b=a     // b=5;
+// // // // // //  a=c     //a=10
+
+// // // // // // console.log("the value of a is " + a)
+// // // // // // console.log("the value of b is " + b)
+
+// // // // // var a = 5 ;
+// // // // // var b = 10 ;
+// // // // // // // output b=5; a=10
+
+// // // // // a = a+b;    //a=15
+// // // // // b=  a-b     //b=5
+// // // // // a= a-b      //a=10
+
+// // // // // console.log("the value of a is " + a)
+// // // // // console.log("the value of b is " + b)
+
+// // // // // var num1=5;
+// // // // // var num2='5';
+// // // // // console.log(typeof(num1));
+// // // // // console.log(typeof(num2));
+// // // // // console.log( num1 == num2 );
+
+// var num1=5;
+// var num2='5';
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log( num1 === num2 );
+
+//  var message = "Thanks for your input!";
+//  alert(message);
+
+// var question = "Your gender?";
+//  var defaultAnswer = "Male/Female";
+//  var spec = prompt(question, defaultAnswer);
+//  if(!spec){
+//     alert("please fill up the field ")
+//     spec = prompt(question, defaultAnswer);
+//  }
+
+// // // // //  var tomr ='rain'
+
+// // // // //  if(tomr == 'rain'){
+// // // // //      console.log('take a raincoat')
+// // // // //  }
+// // // // //  else{
+// // // // //      console.log('no need to take a raincoat')
+// // // // //  }
+
+// // // // // var year = 2020;
+// // // // // debugger;
+// // // // // if(year % 4 === 0){
+// // // // //   if(year % 100 === 0){
+// // // // //     if(year % 400 === 0){
+// // // // //        console.log("the year " + year + " is a leap year")
+// // // // // }else{
+// // // // //     console.log("the year" + year + "is not a leap year")}
+// // // // // }else{
+// // // // //     console.log("the year " + year + " is a leap year")
+// // // // // }
+// // // // // }else{
+// // // // //     console.log("the year" + year + "is not a leap year")
+// // // // // }
+
+// // // // // if(score=0){
+// // // // //     console.log("OMG, we loss the game")
+// // // // // }else{
+// // // // //     console.log("yay, we won the game")
+// // // // // }
+
+// // // // // var age =18
+// // // // // if(age => 18 ){
+// // // // //     console.log("you are eligible to vote")
+// // // // // }else{
+// // // // //     console.log("you are not eligible to vote")
+// // // // // }
+
+// // // // // var age =17
+// // // // // console.log((age >= 18 ) ?  "you can vote": "you can't vote")
+
+// // // //  var area = "circle" ;
+// // // //  var PI =3.142 , l=5, b=4, r=3;
+
+// // // //  switch(area) {
+// // // //      case 'circle':
+// // // //          console.log("the area of circle is:" + PI*r**2);
+// // // //          break;
+// // // //          case 'triangle':
+// // // //             console.log("the area of triangle is:" + (l*b)/2);
+// // // //            break;
+// // // //             case 'rectangle':
+// // // //         console.log("the area of rectangle is:" + (l*b));
+// // // //             break
+// // // //         default:
+// // // //        console.log ("please enter valid data ");
+// // // // }
+
+// // // whilelopp
+// // // var num = 0;
+// // // while(num <=10){
+// // //     console.log(num);
+// // //     num++;
+// // // }
+
+// // Do-whileloop
+// //  var num =0;
+// //  do{
+// //      console.log(num);
+// //      num++;
+// //  }while(num <=10);
+
+// // forloop
+// // for(var num=0; num<=10 ; num++){
+// //     console.log(num);
+// // }
+
+// // java script program to print table for given number (8)?
+// //
+
+// // for(var num=1 ; num<=10;num++){
+// //         var tableof =8
+// //     document.write(tableof + " * " + num + " = " + tableof * num +"<br/>")
+// //         console.log(tableof + " * " + num + " = " + tableof * num)
+// // }
+
+// // funtion definition
+// // calling function
+// // function sum(){ var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`,`waqar`];
+// console.log(myfriends.lastIndexOf("waqar",3))
+
+// //     var a=10,b=20;
+// //     var total=a+b;
+// //     console.log(total)
+// // }
+// // sum()
+
+// // function parameter vs function arguments
+// // function sum(a,b){
+// //     var total=a+b;
+// //     console.log(total)
+// // }
+// // sum(90,30)
+
+// // function expressions
+// // function sum(a,b){
+// //     var total=a+b;
+// //     console.log(total)
+// // }
+// // var funexp=sum(90,30);
+
+// // // return keyword
+// // function sum(a,b){
+// //         return total=a+b;
+// //     }
+// //     var funexp=sum(90,30);
+// //     console.log(funexp);
+
+// // "java script 2015 ecma script es6"
+
+// // LET VS CONST vs var
+
+// // var myname = "Abdul Rahim";
+// // console.log( myname );
+
+// // myname = "Abdul Rahim khan";
+// // console.log( myname );
+
+// // let myname = "Abdul Rahim";
+// // function sum(a,b){
+// //     var total=a+b;
+// //     console.log(total)
+// // }
+// // sum(90,30)
+// // console.log( myname );
+
+// // myname = "Abdul Rahim khan";
+// // console.log( myname );
+
+// // in const you can not change value of variable
+
+// // const myname = "Abdul Rahim";
+// // console.log( myname );
+
+// // let and const work only in curly brasis-block scope
+
+// // function biodata(){
+// //     const myfirstname ="Abdul Rahim";
+// //         console.log(myfirstname);
+// //         if(true){
+// //             const mylastname ="Abdul Rahim Khan";
+// //         console.log("inner "  + myfirstname);
+// //         console.log("inner "  + mylastname);
+// //     }
+// //     console.log("innerouter " + mylastname)
+// // }
+// // biodata()
+
+// var spec = prompt("enter any number");
+// for (var num = 2; num <= spec;num=num+2 ) {
+//     console.log(num);
+//     document.write(num + "<br/>")
+// }
+
+// // var spec = prompt("enter any number");
+// // for (var num = 1; num <= spec; num=num+2) {
+// //          console.log(num);
+// //          document.write(num + "<br/>"   )
+// //        }
+
+//     //  template literals(template strings)
+// // for(var num=1 ; num<=10;num++){
+// //         var tableof =8
+// //         console.log(`${tableof} * ${num} = ${tableof * num}`)
+// // }
+
+// // for(var num=1 ; num<=10;num++){
+// //         var tableof =8
+// //         console.log(`${tableof} * ${num} = ${tableof * num}`)
+// // }
+// // for(var num=1 ; num<=10;num++){
+// //         var tableof =8
+// //         console.log(`${tableof} * ${num} = ${tableof * num}`)
+// // }
+
+// // default perameter
+// // function sum(a,b=5){
+// //     return total=a*b;
+// // }
+// // console.log(sum(3,6))
+
+// // fat arrow function
+// // const sum = () =>`the sum of the two number is ${(a=5)+(b=6)}`;
+// // console.log(sum())
+
+// // Arrays function / storing multiple values in one variable
+// // var myfriends =[`owais`,24,`waqar`,26,`zeeshan`,23];
+// // console.log(myfriends)
+
+// // traversal in array
+// // if we want  to get the single data at a time and also
+// //  if we want to change the data
+// var myfriends =[`owais`,`waqar`,`zeeshan`];
+// console.log(myfriends[2])
+
+// // check the length of array
+//  var myfriends =[`owais`,`waqar`,`zeeshan`];
+//  console.log(myfriends[myfriends.length]);
+//  console.log(myfriends.length);
+
+// // we use for loop to navigate
+
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`];
+// for(var i=0;i<myfriends.length;i++){
+// console.log(myfriends[i]);
+// }
+
+// // for in loop gives index no.  output
+
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`];
+// for(let elements in myfriends ){
+// console.log(elements);
+// }
+
+// // for of loop  give you data of Array
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`];
+// for(let elements of myfriends ){
+// console.log(elements);
+// }
+
+//  foreeach loop calls a function
+//   for each element in the array
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`];
+// myfriends.forEach(function(element,index,array){
+// console.log(element);
+// })
+
+// foeeach loop calls a function
+//  for each element in the array using fat arrow function
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`];
+//   myfriends.forEach((element)=>{
+// console.log(element);
+// })
+
+//  searching and filter in an array #method indexof searh forward
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`,`waqar`];
+// console.log(myfriends.indexOf("waqar"))
+
+// //  searching and filter in an array #method lastIndexof searh backward
+//
+// includes check value is present or not
+// var myfriends =[`owais`,`waqar`,`zeeshan`,`imran`,`waqar`];
+// console.log(myfriends.includes("waqar"))
+
+// find()
+// // price < 400
+// const prices =[300,350,400,200]
+// console.log(prices.find((currVal)=> currVal < 400
+// ))
+
+// // // findIndex()
+// const prices =[300,350,400,200]
+// console.log(prices.findIndex((currVal)=> currVal < 400))
+
+// // Filter()
+// const prices =[250,300,350,400,500,600]
+// const newpricetag=prices.filter((Element,index)=>{
+//     return Element <400
+// })
+//  console.log(newpricetag)
+
+// sort() deals with converting elements into string
+// const months=[`march`,`jan`,`April`,`may`,`nov`]
+// console.log(months.sort())
+// const number=[99,1100,22,19,32]
+// console.log(number.sort())
+
+// CRUD
+// Array.prototype.push()
+// the push method add one or more
+// element to the end of an array and returns the new length of an array
+
+// const animals =['cow','tiger','lion','elephant'];
+// const count=animals.push(`chicken`,'elephant');
+// console.log(animals);
+// console.log(count);
+
+// Array.prototype.unshift()
+// the push method add one or more
+// element to the start of an array and returns the new length of an array
+
+// const animals =['cow','tiger','lion','elephant'];
+// const count=animals.unshift(`chicken`,'elephant');
+// console.log(animals);
+// console.log(count);
+
+// Pop()
+// the pop method removes the last element from an array and returns
+// that element this method changes the length of an array
+
+// const plants=['brocoli','cauliflower','kale','tomato','cabbage'];
+// console.log(plants);
+// console.log(plants.pop());
+// console.log(plants);
+
+// shift()
+// the shift method removes the first element from an array and returns
+// that element this method changes the length of an array
+
+// const plants=['brocoli','cauliflower','kale','tomato','cabbage'];
+// console.log(plants);
+// console.log(plants.shift());
+// console.log(plants);
+
+// splice()
+// Add and removes elements from an array
+// 1.Add dec a the end of the array
+// 2.what is the return value of splice method
+// 3.Update march to March (update)?
+// 4. Delete june from an array?
+
+//  sol-1
+// const months =['january','march','aprail','june','july']
+// const newmonths=months.splice(months.length,0,'December')
+// console.log(months)
+
+// sol-2
+// console.log(newmonths)
+// sol-3
+// const months =['january','march','aprail','june','july']
+// const newmonths=months.splice(1,1,'March')
+// console.log(months)
+
+// sol-4
+// const months =['january','march','aprail','june','july']
+// const newmonths=months.splice(3,1,)
+// console.log(months)
+
+// map()
+// let array1 =[1,2,3,4,5,6]
+// let newarr =array1.map((curElm,index,arr)=>{
+//     return`Index no = ${index} and the value is ${curElm} belong to ${arr}`
+// })
+// console.log(newarr)
+// it return new array without mytating the orignl array
+
+// find the squar root of the element in an array
+// let square=[25,36,49,64,81]
+// let newsquare=square.map((curelm)=>Math.sqrt(curelm));
+//     console.log(newsquare)
+
+// multiply all eloements by 2 and return value only greater than 10
+// let arr =[2,3,4,6,8,];
+// let arr2=arr.map((curelm)=>curelm*2).filter((curelm)=>curelm>10).reduce((curelm,accumulator)=> {
+//     return accumulator+=curelm
+// })
+// console.log(arr2);
+
+// reduce()
+// let arr =[2,3,4,6,8,];
+// let arr2=arr.reduce((accumulator,curelm)=>{
+//     return accumulator+=curelm},7)
+// console.log(arr2)
+
+// "string"
+// var myname='Abdul Rahim'
+// console.log(myname.length)
+// "string"\Escape characters\
+// var myname="Abdul \"Rahim\" khan"
+// console.log(myname)
+
+// if you dont want to mess simply use  the alternate quotes
+// var myname='Abdul "Rahim" khan'
+// console.log(myname)
+// var myfullname=myname.indexOf('khan')
+// console.log(myfullname)
+
+// "string"slice()
+// method selects the element starting at the given start argument,
+// ,and ends at but does not include the given end argument.
+// Note: the orignal array wilol not be changed
+
+// var fruits ="mango,apple,Bananna,kiwi"
+// var newfruits=fruits.slice(0,4)
+// //var newfruits=fruits.slice(6,-2)
+// console.log(newfruits)
+
+// Display only 280 characters of a string like the one used  in twitter
+// var mytweets =
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni nvar Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quo odit laboriosam a consequatur earum dignissimos magni provident et vero quam unde aliquam perferendis dolores aperiam consequuntur ad, ipsum tenetur"
+// console.log(mytweets.length);
+// var tweets280 = mytweets.slice(0, 280);
+// console.log(tweets280.length);
+// console.log(tweets280);
+
+// "string"substring()doesnot accept negative values negative
+// if you give negative value it starts from 0 indexno.
+// method cannot accept negative indexes
+// var fruits ="mango,apple,Bananna,kiwi"
+// var newfruits=fruits.substring(5,-2)
+// console.log(newfruits)
+
+// "string"substr()
+// var fruits ="mango,apple,Bananna,kiwi"
+// var newfruits=fruits.substr(-4)
+// console.log(newfruits)
+
+// "string"replace()
+// the replace method does not change the orignal string and var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+
+
+// replace method replace only the first match and its case sensitivs
+// var myBiodata = "Abdul rahim khan rahim";
+// var replace = myBiodata.replace("rahim", "Rahim");
+// console.log(replace);
+// var replace1 = myBiodata.replace("khan", "Khan");
+// console.log(replace1);
+
+// "String"charAt()
+// extracting character
+// var Myname='Abdul Rahim'
+// console.log(Myname.charAt(4))
+
+// "String"charCodeAt()method returns the unicode of the character at a specified index at a string;
+// the method returns UTF-16 c'aprail','june','july']
+// const newmonths=months.splice(months.length,0,'December')
+// console.log(months)ode
+// (an integer between 0 and 65535)retrn the unicode of the last character of a string 
+
+// var Myname='Abdul Rahim'
+// console.log(Myname.charCodeAt(4))
+// var char='HELLO WORLD';
+// console.log(char.charCodeAt(6))
+
+
+// "String",touppercase(),tolowercase()
+// var Myname='Abdul Rahim'
+// console.log(Myname.toUpperCase())
+// console.log(Myname.toLowerCase())
+
+
+
+// "String",concat()
+// var fname='Abdul'
+// var lname='rahim'
+// console.log(fname +  lname)
+// console.log(`${fname} ${lname}`)//acha tareeka
+// console.log(fname.concat(lname))
+// console.log(fname.concat(' ',lname))
+
+// "String",trim() the string mehod removes whitespace from both sides of a string 
+// var world = "         HELLO WORLD!           "
+// console.log(world)
+// console.log(world.trim())
+
+// Split() 
+// A string can be converted to an Array with the Split method 
+
+// var text='a,b,c,d,e,f'
+// console.log(text.split(","));
+// console.log(text.split(" "));
+// console.log(text.split("|"));
+
+
+// dat and time Object
+// new Date()
+// date objects are created with the new date() constructor
+// first method of getting date and time 
+// let currdate=new Date();
+// console.log(currdate)
+// second method 
+console.log(new Date())
+console.log(new Date().toLocaleString())
+console.log(new Date().toString())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toLowerCase();
+// var cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+// for (var i = 0; i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) {
+// alert("It's one of the cleanest cities");
+// } else {
+//     alert("It's not the cleanest city");
+// }
+// }
+
+// var time=21
+// if (time < 10) {
+//     greeting1 = "Good morning";
+//     console.log(greeting1)
+// } else if (time < 20) {
+//     greeting2 = "Good day";
+//     console.log(greeting2)
+//   } else {
+//     greeting3 = "Good evening";
+//     console.log(greeting3)
+//   }
+
